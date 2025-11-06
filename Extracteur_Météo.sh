ad8aedc9ee10581822100f02a1f5e63a61f71016
Jean Darcy MBONEZANGABO <jean-darcy.mbonezangabo@univ-tlse3.fr>
2025-11-06 23:24:07 +0100

    Modification du temps actuelle

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -6,15 +6,15 @@ DATE=$(date +"%Y-%m-%d")
 HEURE=$(date +"%H:%M")
 
 # 1. Récupération des données météo avec wttr.in
-# (format texte simple)
-METEO_BRUTE=$(curl -s wttr.in/${VILLE}?format=3)
+# (format JSON)
+METEO_BRUTE=$(curl -s wttr.in/${VILLE}?format=j2)
 
 # On veut récupérer la température actuelle
-TEMP_ACTUELLE=$(echo "$METEO_BRUTE" | grep -oE '[+-]?[0-9]+°C')
+TEMP_ACTUELLE=$(echo "$METEO_BRUTE" | grep '"avgtempC":' | head -n 1 | awk -F'"' '{print $4 "°C"}')
 
 # 2. Température prévue pour demain
 # On récupère une ligne spécifique de wttr.in
-PREVISION=$(curl -s wttr.in/${VILLE}?format=j2 | grep -A 20 '"date":' | grep -A 20 "${DATE}" | grep '"avgtempC"' | awk -F'"' '{print $4 "°C"}')
+PREVISION=$(echo "$METEO_BRUTE" | grep -A 20 '"date":' | grep -A 20 "${DATE}" | grep '"avgtempC"' | awk -F'"' '{print $4 "°C"}')
 
 # 3. Enregistrer les données dans meteo.txt
 LIGNE="${DATE} - ${HEURE} - ${VILLE} : ${TEMP_ACTUELLE} - ${PREVISION}" 
