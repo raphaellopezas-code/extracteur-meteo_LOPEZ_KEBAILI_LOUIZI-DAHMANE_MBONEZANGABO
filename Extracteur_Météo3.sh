d8147bc4247dd2fb157c90bacce6ae5aad396cc6
Hinet <kebailihouria@MacBook-Air-de-Hinet.local>
2025-11-15 14:22:15 +0100

    Ajouter la ligne dans le fichier du jour

--- Extracteur_Météo3.sh
+++ Extracteur_Météo3.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 # version 3 Gestion d’historique
+#Toulouse par défaut (aucun arguement fourni)
+VILLE=${1:-Toulouse}
 #je crée un nom de fichier qui dépend de la date du jour 
 Jour=$(date +"%Y%m%d")
-fichier="meteo_${Jour}.txt"
+fichier_historique="meteo_${Jour}.txt"
+#je récupert la méteo 
+tmp_aujourdhui=$(curl -s "wttr.in/$VILLE?format=%t")
+tmp_demain=$(curl -s "wttr.in/$VILLE?format=%t&tomorrow")
+#ajout la ligne dans le fichier histrorique 
+echo"$(date"+%y-%d-%d:%m")$meteo >> "$fichier"
+
