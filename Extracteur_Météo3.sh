27140adb67c6ba0476bee27919fa5745a6a92060
Hinet <kebailihouria@MacBook-Air-de-Hinet.local>
2025-11-15 16:41:12 +0100

    Modification du script en modifiant l'ajout dans l'historique

--- Extracteur_Météo3.sh
+++ Extracteur_Météo3.sh
@@ -2,12 +2,18 @@
 # version 3 Gestion d’historique
 #Toulouse par défaut (aucun arguement fourni)
 VILLE=${1:-Toulouse}
+#Date et heure actuelles
+DATE=$(date +"%Y-%m-%d")
+HEURE=$(date +"%H:%M")
 #je crée un nom de fichier qui dépend de la date du jour 
 Jour=$(date +"%Y%m%d")
 fichier_historique="meteo_${Jour}.txt"
 #je récupert la méteo 
 tmp_aujourdhui=$(curl -s "wttr.in/$VILLE?format=%t")
 tmp_demain=$(curl -s "wttr.in/$VILLE?format=%t&tomorrow")
+
+
 #ajout la ligne dans le fichier histrorique 
-echo"$(date"+%y-%d-%d:%m")$meteo >> "$fichier"
+echo"$DATE - $HEURE - $VILLE : $tmp_aujourdhui - $tmp_demain" >> "$fichier_historique"
+
 
