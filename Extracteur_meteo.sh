a6d82ce1093eb1b326585fef483ea95481abd558
khaoula-louizi <hibalouizi@icloud.com>
2025-11-16 14:49:29 +0100

    VARIANTE 1 : script meteo , vent , humidité et visibilité

--- Extracteur_meteo.sh
+++ Extracteur_meteo.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-echo "Scrip météo - version de base"
-if [ -z "$1" ];
-then
-   echo "Usage = $0 <Ville>"
-   exit 1
+
+if [ -z "$1" ]; then
+   echo "Usage : $0 <Ville>"
+    exit 1
+
 fi
 
-VILLE =$1
+VILLE=${1:-Toulouse}
+
+DATE=$(date +"%Y-%m-%d")
+HEURE=$(date +"%H:%M")
+
+FICHIER="meteo_${DATE}.txt"
+
+INFO=$(curl -s "wttr.in/${VILLE}?format=%t+%C+%w+%h+%v")
+
+LIGNE="${DATE} ${HEURE} ${VILLE} : ${INFO}"
+
+echo "$LIGNE" >> "$FICHIER"
+
+echo "Données enregistrées : $LIGNE"
