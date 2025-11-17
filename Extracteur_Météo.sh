649d24068988306649fc7081428dee7c78c5af9f
raph <raphael.lopez@gmail.com>
2025-11-17 16:27:03 +0100

    Modification de la version où il faut obligatoirement une ville en entrée

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
+
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <ville>"
     exit 1
 fi
+
 VILLE=$1
-FICHIER_TEMP="meteo_brut.txt"
-> "$FICHIER_TEMP"
 
-curl -s "wttr.in/${VILLE}" -o "$FICHIER_TEMP"
+METEO=$(curl -s "wttr.in/${VILLE}" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
 
-sed -i 's/\x1B\[[0-9;]*[JKmsu]//g' "$FICHIER_TEMP"
-
-TEMP_ACTUELLE=$(grep -o '[+-]\?[0-9]\+' "$FICHIER_TEMP" | head -1 | sed 's/^+//')
-
+TEMP_ACTUELLE=$(echo "$METEO" | grep -o '[+-]\?[0-9]\+°C' | head -1)
 
 DATE_DEMAIN=$(date -d tomorrow "+%a %d %b")
-TEMP_PREVUE=$(grep -A5 "$DATE_DEMAIN" "$FICHIER_TEMP" | grep -o '[+-]\?[0-9]\+' | head -2 | tail -1 | sed 's/^+//')
+TEMP_DEMAIN=$(echo "$METEO" | grep -A8 "$DATE_DEMAIN" | grep -o '[+-]\?[0-9]\+°C' | head -1)
 DATE_COURANTE=$(date +"%Y-%m-%d - %H:%M")
 
-[ ! -f "meteo.txt" ] && touch "meteo.txt"
-
-
-echo "${DATE_COURANTE} - ${VILLE} : ${TEMP_ACTUELLE}°C - ${TEMP_PREVUE}°C" >> "meteo.txt"
+echo "${DATE_COURANTE} - ${VILLE} : ${TEMP_ACTUELLE} - ${TEMP_DEMAIN}"
 
